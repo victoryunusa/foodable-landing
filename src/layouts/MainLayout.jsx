62686592e2888da4dfb7c1c26a6ef5e360927517
Yunusa Victor <victoryunusa@Yunusas-MacBook-Pro.local>
2022-03-13 14:55:20 +0100

    Removed: max width from layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 
 const MainLayout = () => {
   return (
-    <div className="max-w-screen min-h-full font-open text-neutral-700">
+    <div className="min-w-full min-h-full font-open text-neutral-700">
       <div className="bg-teal-900 p-4">
         <Header />
       </div>
